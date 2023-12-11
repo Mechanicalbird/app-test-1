bc4fa44867c172c0b06d5ff9a85b934593dcd1db
Mechanicalbird <mechanicalbird_just@yahoo.com>
2023-12-11 17:36:08 +0900

    Update main_live.py

--- main_live.py
+++ main_live.py
@@ -1,23 +1,26 @@
 import numpy as np
 import cv2
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-class VideoTransformer(VideoTransformerBase):
+class Faceemotion(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
+
         return img
 
 def main():
     # Face Analysis Application #
-    st.title("Real Time Face Application")
+    st.title("Real Time sizing Application")
+    
     st.header("Webcam Live Feed")
-    
     st.write("Click on start to use webcam and detect your face emotion")
-    
-    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+    webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
+                        video_processor_factory=Faceemotion)
 
-    
+
+
 if __name__ == "__main__":
     main()
